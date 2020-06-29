fd76e21e356ec4ffda34bcb82924c520d7c16ca2
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-06-29 18:45:11 +0700

    fix white box

--- src/script/league-dom.js
+++ src/script/league-dom.js
@@ -8,16 +8,20 @@ export default function leagueDOM({competition: cp, season: se, standings: st}){
             <div class="container pb10">
                 <div class="row white-text right-align s-center-align">
                     <div class="col s12">
-                        <h1>${cp.name}</h1>
+                        <h1 class="mb10">${cp.name}</h1>
                     </div>
                 </div>
                 <div class="row white-text">
-                    <div class="col s2 right-align right">
-                        <p>${cp.area.name}</p>
+                    <div class="col s12 l2 right-align s-center-align right">
+                        <div class="box-container flex-end s-flex-center pb20">
+                            <div class="white-box">
+                                <p>${cp.area.name}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col s10 left-align left">
-                        <p class="info">Updated: ${time.substring(0,10)} at ${time.substring(11,16)} GMT</p>
-                        <p class="info">Event Ends: ${se.endDate}</p>
+                    <div class="col s12 l10 left-align left">
+                        <p class="info"><b>Updated:</b> ${time.substring(0,10)} at ${time.substring(11,16)} GMT</p>
+                        <p class="info"><b>Event Ends:</b> ${se.endDate}</p>
                     </div>
                 </div>
             </div>
