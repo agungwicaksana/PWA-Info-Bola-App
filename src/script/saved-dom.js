082c783ec49f48f3538bd83b34877f3a8238929d
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-07 11:54:34 +0700

    selesai membuat tampilan saved team. next: membuat navigasi ketika saved team diklik

--- src/script/saved-dom.js
+++ src/script/saved-dom.js
@@ -1,7 +1,61 @@
+import urlRplc from "./url-https.js";
+
 export default function savedTeams() {  
     console.log('saveddom js')
-    getAll().then(team => {
-        console.log('team',team)
+    getAll().then(teams => {
+        console.log('teams',teams)
+
+        const container = document.getElementById(`saved-container`);
+        let htmlSaved = '';
+        const headContent = `
+            <div class="light-blue darken-3">
+                <div class="container pb10">
+                    <div class="row white-text right-align s-center-align">
+                        <div class="col s12">
+                            <h1 class="mb10">Tim Favorit</h1>
+                        </div>
+                        <div class="col s12">
+                            <p class="info">Anda memiliki ${teams.length} tim favorit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        htmlSaved += headContent;
+
+        const savedTeamsContainer = `
+            <div class="container">
+                <div class="row" id="saved-teams-container">
+                </div>
+            </div>
+        `;
+        htmlSaved += savedTeamsContainer;
+
+        container.innerHTML = htmlSaved;
+
+        let teamComp = ''; 
+        teams.forEach(team => {
+            console.log('team', team)
+            teamComp += `
+                <a href="#team/${team.id}" class="team-detail">
+                    <div class="col s12 m10 l8 offset-m1 offset-l2">
+                        <div class="team-item waves-effect">
+                            <img src="${urlRplc(team.crestUrl)}" alt="Logo ${team.name}" onerror="this.onerror=null;this.src='assets/icon.svg'">
+                            <div class="team-item-content">
+                                <span class="badge black-text">${team.tla}</span>
+                                <span>${team.name}</span>
+                                <hr/>
+                                <div>
+                                    <span class="stat teal-text">
+                                        ${team.venue} - ${team.area.name}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            `
+        });
+        document.getElementById('saved-teams-container').innerHTML = teamComp;
     })
-    
 }
