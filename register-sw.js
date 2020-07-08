ef0d1bf36d606f9d5ae2aa2737fa52ba731d372d
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-08 14:00:29 +0700

    on process fcm

--- register-sw.js
+++ register-sw.js
@@ -1,7 +1,12 @@
 if (!("serviceWorker" in navigator)) {
     console.error("ServiceWorker: Browser tidak mendukung")
 } else {
-    navigator.serviceWorker
+    registerServiceWorker();
+    requestNotifPermission();
+}
+
+function registerServiceWorker() {
+    return navigator.serviceWorker
         .register("/service-worker.js")
         .then(function(registration) {
             console.log("ServiceWorker: Pendaftaran berhasil. Scope: ", registration.scope);
@@ -9,4 +14,29 @@ if (!("serviceWorker" in navigator)) {
         .catch(function(error) {
             console.log("ServiceWorker: Pendaftaran Gagal. Error: ", error);
         })
+}
+
+function requestNotifPermission() {
+    if ('Notification' in window) {
+        Notification.requestPermission().then(function (result) {
+            if (result === "granted") {
+                if (('PushManager' in window)) {
+                    navigator.serviceWorker.getRegistration().then(function(registration) {
+                        registration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: << App Server Key >>
+                        }).then(function(subscribe) {
+                            console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
+                            console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
+                                null, new Uint8Array(subscribe.getKey('p256dh')))));
+                            console.log('Berhasil melakukan subscribe dengan auth key: ', btoa(String.fromCharCode.apply(
+                                null, new Uint8Array(subscribe.getKey('auth')))));
+                        }).catch(function(e) {
+                            console.error('Tidak dapat melakukan subscribe ', e.message);
+                        });
+                    });
+                }
+            }
+        });
+    }
 }
