179a5146fe9216923492104edb98b5379e2d136f
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-08 17:19:44 +0700

    berhasil subscribe FCM

--- register-sw.js
+++ register-sw.js
@@ -1,3 +1,5 @@
+import urlBase64ToUint8Array from "./src/script/uint8AConverter.js";
+
 if (!("serviceWorker" in navigator)) {
     console.error("ServiceWorker: Browser tidak mendukung")
 } else {
@@ -24,7 +26,7 @@ function requestNotifPermission() {
                     navigator.serviceWorker.getRegistration().then(function(registration) {
                         registration.pushManager.subscribe({
                             userVisibleOnly: true,
-                            applicationServerKey: << App Server Key >>
+                            applicationServerKey: urlBase64ToUint8Array("BOMAy8Ecr-5g8ud64Bwl4rh89FuaQs34VLKpHMc2AJG31IWgoKOrcYBL8eL34h_VN_k5CPdbFQsdwxitpEl4naU")
                         }).then(function(subscribe) {
                             console.log('Berhasil melakukan subscribe dengan endpoint: ', subscribe.endpoint);
                             console.log('Berhasil melakukan subscribe dengan p256dh key: ', btoa(String.fromCharCode.apply(
