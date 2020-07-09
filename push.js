434b69bafb7225465645388f4788a769c6e83ff1
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-07-09 09:56:07 +0700

    edit push js

--- push.js
+++ push.js
@@ -1,21 +1,20 @@
 const webPush = require('web-push');
- 
+
 const vapidKeys = {
     "publicKey": "BOMAy8Ecr-5g8ud64Bwl4rh89FuaQs34VLKpHMc2AJG31IWgoKOrcYBL8eL34h_VN_k5CPdbFQsdwxitpEl4naU",
     "privateKey": "MOEDS-v0FF6R00RW6Cjp02yJzjKYBJRlnUuU6xt-YwQ"
 };
- 
- 
+
 webPush.setVapidDetails(
     'mailto:agung.wicaksaana@gmail.com',
     vapidKeys.publicKey,
     vapidKeys.privateKey
 )
 const pushSubscription = {
-    "endpoint": "endpoint",
+    "endpoint": "endpoint (ambil dari console log pada aplikasi)",
     "keys": {
-        "p256dh": "p256dh",
-        "auth": "authKey"
+        "p256dh": "p256dh (ambil dari console log pada aplikasi)",
+        "auth": "authKey (ambil dari console log pada aplikasi)"
     }
 };
 const payload = 'Ini Notifikasi dari FCM!';
